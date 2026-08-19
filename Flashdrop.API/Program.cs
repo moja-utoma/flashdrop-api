dbbafe2af410608195d9fb285afa9e90d715d020
moja-utoma <ryabova.yelizaveta@gmail.com>
2026-08-19 12:34:27 +0300

    chore(infra): set up swagger, redis and rabbitmq connection
    Closes #1

--- Flashdrop.API/Program.cs
+++ Flashdrop.API/Program.cs
@@ -1,3 +1,5 @@
+using RabbitMQ.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,36 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+builder.Services.AddSingleton<IConnectionFactory>(sp =>
+{
+    var config = builder.Configuration.GetSection("RabbitMq");
+    return new ConnectionFactory
+    {
+        HostName = config["HostName"],
+        Port = int.Parse(config["Port"]!),
+        UserName = config["UserName"],
+        Password = config["Password"]
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Flashdrop API v1");
+    });
 }
 
 app.UseHttpsRedirection();
