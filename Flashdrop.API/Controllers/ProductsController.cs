2683e21882c9e7ebe5848ded86a346be5400cdc6
moja-utoma <ryabova.yelizaveta@gmail.com>
2026-08-27 10:56:32 +0300

    chore(infra): add application layer with basic structure

---
+++ Flashdrop.API/Controllers/ProductsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flashdrop.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+}
